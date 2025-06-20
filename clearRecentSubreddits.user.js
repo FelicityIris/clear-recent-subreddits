c84a41d7def6e9d578f448ee2e35146b7e582f80
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2025-06-21 00:30:08 +0530

    feat: add userscript

---
+++ clearRecentSubreddits.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name		clearRecentSubreddits
+// @namespace	https://reddit.com/
+// @version		1.0
+// @description	Deletes 'recent-subreddits-store' from localStorage on reddit.com
+// @author		Felicity Iris (github.com/felixity1917)
+// @match		https://www.reddit.com/*
+// @grant		none
+// @run-at		document-start
+// @license		GPL-3.0
+// @homepage	https://github.com/felixity1917/clear-recent-subreddits
+// @downloadURL	https://raw.githubusercontent.com/felixity1917/clear-recent-subreddits/refs/heads/main/clearRecentSubreddits.user.js
+// @updateURL	https://raw.githubusercontent.com/felixity1917/clear-recent-subreddits/refs/heads/main/clearRecentSubreddits.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const key = 'recent-subreddits-store';
+
+    const clearKey = () => {
+        if (localStorage.getItem(key)) {
+            localStorage.removeItem(key);
+            console.log(`Removed ${key}`);
+        }
+    };
+
+    // Initial clear after DOM load
+    window.addEventListener('DOMContentLoaded', () => {
+        clearKey();
+
+        // Re-check periodically in case Reddit resets it
+        setInterval(clearKey, 1000); // Adjust interval if needed
+    });
+})();
